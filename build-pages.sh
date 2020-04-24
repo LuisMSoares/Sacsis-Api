edb1b7c2e61b2bcd2ca97247553b4cbe743ae099
LuisMSoares <luisedu.soares@outlook.com>
2020-04-24 20:01:24 -0300

    configurações para o deploy da aplicação foram movidas para um novo repositorio

--- build-pages.sh
+++
@@ -1,9 +0,0 @@
-rm -rf sacsis.tech
-git clone -b builded https://github.com/iguit0/xi-sacsis-website.git sacsis.tech
-cp -a ./sacsis.tech/dist/* ./sacsis.tech
-rm -rf ./sacsis.tech/dist
-
-rm -rf app.sacsis.tech
-git clone -b builded https://github.com/iguit0/xi-sacsis-admin app.sacsis.tech
-cp -a ./app.sacsis.tech/dist/* ./app.sacsis.tech
-rm -rf ./app.sacsis.tech/dist
